0551d1b1621799f8c5e50cc2dabfa137074f8105
zwt <1295125020@qq.com>
2022-12-13 17:30:14 +0800

    finish tile_map

--- board_plugin/src/resources/tile.rs
+++ board_plugin/src/resources/tile.rs
@@ -21,8 +21,16 @@ impl Tile {
     }
 
     #[cfg(feature = "debug")]
-    pub fn console_output() -> String {
-				// format!({}, match )
-        todo!()
+    pub fn console_output(&self) -> String {
+        format!({}, match self {
+            Tile::Bomb => "*".bright_red(),
+            Tile::BombNeighbor(num) => match {
+                1 => "1".cyan(),
+                2 => "2".green(),
+                3 => "3".yellow(),
+                _ => v.to_string().red()
+            },
+            Tile::Empty => " ".normal()
+        })
     }
 }
---
+++ board_plugin/src/resources/tile_map.rs
@@ -0,0 +1,69 @@
+use super::tile::Tile;
+use std::ops::{Deref, DerefMut};
+
+pub struct TileMap {
+    bomb_count: u16,
+    height: u16,
+    width: u16,
+    map: Vec<Vec<Tile>>,
+}
+
+impl TileMap {
+    pub fn new(height: u16, width: u16) -> Self {
+        let map = (0..height)
+            .map(|_| (0..width).map(|_| Tile::Empty).collect())
+            .collect();
+
+        Self {
+            bomb_count: 0,
+            height,
+            width,
+            map,
+        }
+    }
+
+    pub fn height(&self) -> u16 {
+        self.height
+    }
+
+    pub fn width(&self) -> u16 {
+        self.width
+    }
+
+    pub fn bomb_count(&self) -> u16 {
+        self.bomb_count
+    }
+
+    #[cfg(feature = "debug")]
+    pub fn console_output(&self) -> String {
+        let mut buffer = format!(
+            "Map ({}, {}) with {} bombs:\n",
+            self.width, self.height, self.bomb_count
+        );
+        let line = (0..(self.width + 1)).map(|_| '-').collect::<String>();
+        buffer = format!("{}{}\n", buffer, line);
+
+        for line in self.iter() {
+            buffer = format!("{}|", buffer);
+            for tile in line.iter() {
+                buffer = format!("{}{}", buffer, tile.console_output());
+            }
+            buffer = format!("{}|\n", buffer);
+        }
+        format!("{}{}", buffer, line)
+    }
+}
+
+impl Deref for TileMap {
+    type Target = Vec<Vec<Tile>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.map
+    }
+}
+
+impl DerefMut for TileMap {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.map
+    }
+}
